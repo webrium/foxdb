5405d79612ba81e5b51daaf605fd490e88bd0b7a
سید بنیامین خلیفه <nvioir@gmail.com>
2020-12-09 12:05:25 +0330

    between test

--- tests/between.php
+++ tests/between.php
@@ -4,24 +4,26 @@ require_once __DIR__ . '/config.php';
 use webrium\foxql\db;
 
 
+echo "between:<br>";
+
 $res = DB::table('users')->between('age',[16,20])->get();
 echo json_encode($res);
 
-echo "<br>===========<br>";
+echo "<br>===========<br>orBetween:<br>";
 
 $res = DB::table('users')
 ->where('name','ben')
 ->orBetween('age',[18,20])->get();
 echo json_encode($res);
 
-echo "<br>===========<br>";
+echo "<br>===========<br>notBetween:<br>";
 
 $res = DB::table('users')
 ->where('name','ben')
 ->notBetween('age',[18,20])->get();
 echo json_encode($res);
 
-echo "<br>===========<br>";
+echo "<br>===========<br>orNotBetween:<br>";
 
 $res = DB::table('users')
 ->where('name','ben')
