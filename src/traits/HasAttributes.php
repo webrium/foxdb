17b4afe2192bf088783dc2e6d4f6d48b99b41d82
Aether <87689344+SwarooopDev@users.noreply.github.com>
2022-10-15 23:35:53 +0530

    Trait #1

--- src/traits/HasAttributes.php
+++ src/traits/HasAttributes.php
@@ -1,1 +1,33 @@
-//TODO
+<?php
+
+namespace Foxdb\traits;
+
+trait HasAttributes
+{
+    /**
+         * The model's attributes.
+              *
+                   * @var array
+                        */
+                            protected $attributes = [];
+
+                                /**
+                                     * The model attribute's original state.
+                                          *
+                                               * @var array
+                                                    */
+                                                        protected $original = [];
+
+                                                            /**
+                                                                 * The changed model attributes.
+                                                                      *
+                                                                           * @var array
+                                                                                */
+                                                                                    protected $changes = [];
+
+                                                                                        /**
+                                                                                             * The attributes that should be cast.
+                                                                                                  *
+                                                                                                       * @var array
+                                                                                                            */
+                                                                                                                protected $casts = [];
