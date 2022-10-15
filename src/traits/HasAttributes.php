d3a4adbcf13bd2eb5956ee3365c72612d66b33e2
Aether <87689344+SwarooopDev@users.noreply.github.com>
2022-10-15 23:39:02 +0530

    First function in HasAttributes.
    TODO: add some functions like Laravel

--- src/traits/HasAttributes.php
+++ src/traits/HasAttributes.php
@@ -5,29 +5,34 @@ namespace Foxdb\traits;
 trait HasAttributes
 {
     /**
-         * The model's attributes.
-              *
-                   * @var array
-                        */
-                            protected $attributes = [];
+     * The model's attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
 
-                                /**
-                                     * The model attribute's original state.
-                                          *
-                                               * @var array
-                                                    */
-                                                        protected $original = [];
+    /**
+     * The model attribute's original state.
+     *
+     * @var array
+     */
+    protected $original = [];
 
-                                                            /**
-                                                                 * The changed model attributes.
-                                                                      *
-                                                                           * @var array
-                                                                                */
-                                                                                    protected $changes = [];
+    /**
+     * The changed model attributes.
+     *
+     * @var array
+     */
+    protected $changes = [];
 
-                                                                                        /**
-                                                                                             * The attributes that should be cast.
-                                                                                                  *
-                                                                                                       * @var array
-                                                                                                            */
-                                                                                                                protected $casts = [];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    public function getAttribute($key){
+        return $this->attributes[$key];
+    }
+}
